2fccf9278e7115305811eda2d2b34b8c730fc4c8
Clay Branch <clay.branch@gmail.com>
2016-03-08 15:34:10 -0600

    added reducer for checking if a link is being editted

--- client/reducers/links.js
+++ client/reducers/links.js
@@ -6,13 +6,19 @@ const isLinkInfoLoading = createReducer(false, {
   REQUEST_LINK_INFO: () => true,
 });
 
+const isLinkBeingEditted = createReducer(false, {
+  RECEIVE_LINK_INFO: () => true,
+});
+
 const currentLink = createReducer(null, {
   RECEIVE_LINK_INFO: (state, action) => {
     return Immutable.fromJS(action.data);
   },
 });
 
+
 module.exports = {
+  isLinkBeingEditted,
   isLinkInfoLoading,
   currentLink,
 };
--- test/client/reducers/links_test.js
+++ test/client/reducers/links_test.js
@@ -1,12 +1,13 @@
 /* global TEST_HELPER describe it it_ TestHelper __server __client beforeEach expect */
 'use strict';
+const Immutable = require('immutable');
 require(TEST_HELPER);
-const { isLinkInfoLoading, currentLink } = require(`${__client}/reducers/links`);
-const { requestLinkInfo, receiveLinkInfo } = require(`${__client}/actionCreators/links`);
+const { isLinkInfoLoading, currentLink, isLinkBeingEditted } = require(`${__client}/reducers/links`);
+const { requestLinkInfo, receiveLinkInfo, requestNewLink } = require(`${__client}/actionCreators/links`);
 describe('The Link Info Loading Reducer', () => {
   it('sets link info as loading after requesting link info', () => {
-    const state = isLinkInfoLoading(false, requestLinkInfo());
-    expect(state[0]).to.equal = true;
+    const state = isLinkInfoLoading(undefined, requestLinkInfo());
+    state.should.equal(true);
   });
 });
 
@@ -17,5 +18,12 @@ describe('The currentLink reducer', () => {
     };
     const state = currentLink(null, receiveLinkInfo(linkData));
     expect(state.toJS()).to.contain(linkData);
+  });
+});
+
+describe('The isLinkBeingEditted reducer', () => {
+  it('sets link being editted as true when link info is recieved', () => {
+    const state = isLinkBeingEditted(undefined, receiveLinkInfo({}));
+    state.should.equal(true);
   });
 });
