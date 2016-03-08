74fa493a7f5e8ccb734ab7da3bfcf7a5cf332075
Clay Branch <clay.branch@gmail.com>
2016-03-08 11:11:14 -0600

    added actions for getting link info

--- client/actionCreators/links.js
+++ client/actionCreators/links.js
@@ -1,20 +1,40 @@
+const { getAsyncAction } = require('../lib/redux-helpers');
 require('es6-promise').polyfill();
-const fetch = require('isomorphic-fetch');
+const Link = require('../models/link');
 
 function requestLinkInfo(url) {
   return {
     type: 'REQUEST_LINK_INFO',
+    url,
   };
 }
 
-function requestNewTodo(text) {
+function receiveLinkInfo(data) {
   return {
-    type: 'REQUEST_NEW_TODO',
-    id: 'creating',
-    text,
+    type: 'RECEIVE_LINK_INFO',
+    data,
   };
+}
+
+function receiveLinkInfoError(error) {
+  return {
+    type : 'RECEIVE_LINK_INFO_ERROR',
+    error,
+  };
+}
+
+function getLinkInfo(url) {
+  return dispatch => getAsyncAction({
+    dispatch,
+    promise: Link.getInfo(url),
+    initial: () => requestLinkInfo(url),
+    success: (info) => receiveLinkInfo(info),
+    error: (error) => receiveLinkInfoError(error),
+  });
 }
 
 module.exports = {
-
+  requestLinkInfo,
+  receiveLinkInfo,
+  getLinkInfo,
 };
