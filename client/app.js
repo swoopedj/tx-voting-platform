75441cb5e1413ea41c7d23b417457aad8dd2934a
Clay Branch <clay.branch@gmail.com>
2016-03-10 13:24:55 -0600

    removed link route and and loaded HomeRoute

--- client/app.js
+++ client/app.js
@@ -1,9 +1,9 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const Provider = require('react-redux').Provider;
-const App = require('./containers/App');
-const CreateLinkRoute = require('./containers/CreateLinkRoute');
-const Home = require('./components/app/Home');
+require('./lib/longStackTraces.js');
+import App from './containers/App';
+import HomeRoute from './containers/HomeRoute';
 const { Router, Route, IndexRoute } = require('react-router');
 const configureStore = require('./store/configureStore');
 const configureHistory = require('./lib/configureHistory');
@@ -14,8 +14,7 @@ ReactDOM.render(
 	<Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
-        <IndexRoute components={{ main: Home }} />
-        <Route path="/create" components={{ main: CreateLinkRoute }} />
+        <IndexRoute components={{ main: HomeRoute }} />
       </Route>
     </Router>
   </Provider>,
