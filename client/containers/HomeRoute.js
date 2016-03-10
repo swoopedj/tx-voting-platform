54cf2e2f7e112ba5da67c316eb4848a6a5c883fc
Clay Branch <clay.branch@gmail.com>
2016-03-10 13:31:02 -0600

    added home route to handle functionality that needs to happen when the route loads

---
+++ client/containers/HomeRoute.js
@@ -0,0 +1,36 @@
+import React, { Component, PropTypes } from 'react';
+import Home from '../components/app/Home';
+import { connect } from 'react-redux';
+import actions from '../actionCreators/links';
+
+class HomeRoute extends Component {
+  componentDidMount() {
+    const { fetchLinks } = this.props;
+    fetchLinks();
+  }
+  render() {
+    return <Home {...this.props} />;
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { isLoading, items, error } = state.toJS().links;
+  return {
+    isLoading,
+    links: items,
+    error,
+  };
+};
+
+const matchDispatchToProps = (dispatch) => {
+  return {
+    fetchLinks: () => dispatch(actions.fetchLinks()),
+  };
+};
+
+
+HomeRoute.propTypes = {
+  fetchLinks: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, matchDispatchToProps)(HomeRoute);
