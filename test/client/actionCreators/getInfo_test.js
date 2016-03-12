5e0e6c6e50ddddb152e6d40c5afdf776f033b130
Clay Branch <clay.branch@gmail.com>
2016-03-12 12:27:59 -0600

    reworking test structure

--- test/client/actionCreators/getInfo_test.js
+++ test/client/actionCreators/getInfo_test.js
@@ -1,4 +1,4 @@
-/* global TEST_HELPER describe it it_ TestHelper __server __client beforeEach expect */
+/* global TEST_HELPER describe it it_ TestHelper __server __client beforeEach afterEach expect */
 'use strict';
 require(TEST_HELPER);
 const Entry = require(`${__client}/models/entry`);
@@ -12,42 +12,42 @@ const sinon = require('sinon');
 require('sinon-as-promised');
 
 describe('The getLinkInfo async action', () => {
+  const infoResponse = {
+    test: 1,
+  };
+  const error = {
+    message: 'test',
+  };
+  let dispatchSpy = null;
+  let getInfoStub = null;
+  const url = 'http://google.com';
+  const requestAction = requestEntryInfo(url);
+  const errorAction = receiveEntryInfoError(error);
+  const receiveAction = receiveEntryInfo(infoResponse);
+  const dispatchLinkInfo = getEntryInfo('http://google.com');
+  beforeEach(() => {
+    dispatchSpy = sinon.spy();
+    getInfoStub = sinon.stub(Entry, 'getInfo');
+  });
+
+  afterEach(() => {
+    getInfoStub.restore();
+  });
+
   it_('dispatches receive link info on success', function * testAction() {
-    const getInfo = sinon.stub(Entry, 'getInfo');
-    const url = 'http://google.com';
-    const requestAction = requestEntryInfo(url);
-    const infoResponse = {
-      test: 1,
-    };
-    const receiveAction = receiveEntryInfo(infoResponse);
-    getInfo.resolves(infoResponse);
-    const dispatchSpy = sinon.spy();
-    const dispatchLinkInfo = getEntryInfo('http://google.com');
+    getInfoStub.resolves(infoResponse);
     yield dispatchLinkInfo(dispatchSpy);
     // confirm that the request action was dispatched first
     expect(dispatchSpy.firstCall.calledWith(requestAction)).to.equal(true);
-
     // confirm that the receive action was dispatched next
     expect(dispatchSpy.secondCall.calledWith(receiveAction)).to.equal(true);
-    getInfo.restore();
   });
   it_('dispatches link info error on failure', function * testAction() {
-    const getInfo = sinon.stub(Entry, 'getInfo');
-    const url = 'http://google.com';
-    const requestAction = requestEntryInfo(url);
-    const error = {
-      message: 'test',
-    };
-    const errorAction = receiveEntryInfoError(error);
-    getInfo.rejects(error);
-    const dispatchSpy = sinon.spy();
-    const dispatchLinkInfo = getEntryInfo('http://google.com');
+    getInfoStub.rejects(error);
     yield dispatchLinkInfo(dispatchSpy);
     // confirm that the request action was dispatched first
     expect(dispatchSpy.firstCall.calledWith(requestAction)).to.equal(true);
-
     // confirm that the receive action was dispatched next
     expect(dispatchSpy.secondCall.calledWith(errorAction)).to.equal(true);
-    getInfo.restore();
   });
 });
