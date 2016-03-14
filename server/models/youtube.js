c79488d9b7a5690ca2174c82c34631a270233719
Dylan Swoope <swoopedj@gmail.com>
2016-03-14 11:36:18 -0500

    fixed linting errors

--- server/models/youtube.js
+++ server/models/youtube.js
@@ -28,26 +28,24 @@ Youtube.getInfo = (url) => {
   });
 };
 
-Youtube.getBatchInfo = (urlArray) => {
-  //youtube accepts up to 50 id requests at a time
-  //create batch request by making get request with multiple appended ids
-  //in url first, then do batching
-  const base = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics,player&id=';
+// Youtube.getBatchInfo = (urlArray) => {
+//   //youtube accepts up to 50 id requests at a time
+//   //create batch request by making get request with multiple appended ids
+//   //in url first, then do batching
+//   const base = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics,player&id=';
 
-  const idString = urlArray.map((url) => {
-    return Url.parse(fullUrl, true).query.v;
-  }).join(',');
+//   const idString = urlArray.map((url) => {
+//     return Url.parse(fullUrl, true).query.v;
+//   }).join(',');
 
-  var cleanUrl = `${base}${idString}&key=${process.env.YOUTUBE_API_KEY}`;
+//   var cleanUrl = `${base}${idString}&key=${process.env.YOUTUBE_API_KEY}`;
 
-  return Request.fetch(cleanUrl)
-  .then((response) => {
-    return response;
-  })
-  .catch((error) => {
-    throw new Error('Something went wrong with the provided urls:', error);
-  })
+//   return Request.fetch(cleanUrl)
+//   .then((response) => {
+//     return response;
+//   })
+//   .catch((error) => {
+//     throw new Error('Something went wrong with the provided urls:', error);
+//   })
 
-}
-
-
+// }
