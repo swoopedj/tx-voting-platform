bde6033aabb8e5a433541a1404cc8b350bc70c1b
Clay Branch <clay.branch@gmail.com>
2016-03-14 11:51:00 -0500

    first refactor test passing

---
+++ client/reducers/entryInfo.js
@@ -0,0 +1,24 @@
+import Immutable from 'immutable';
+import { createReducer } from '../lib/redux-helpers';
+import { combineReducers } from 'redux-immutable';
+
+const data = createReducer(Immutable.fromJS({}), {
+  RECEIVE_ENTRY_INFO: (state, action) => Immutable.fromJS(action.data),
+});
+
+const isFetching = createReducer(true, {
+  REQUEST_ENTRY_INFO: () => true,
+  RECEIVE_ENTRY_INFO: () => false,
+  RECEIVE_ENTRY_INFO_ERROR: () => false,
+});
+
+const error = createReducer(null, {
+  RECEIVE_ENTRY_INFO: () => null,
+  RECEIVE_ENTRY_INFO_ERROR: (state, action) => Immutable.fromJS(action.error),
+});
+
+export const info = combineReducers({
+  data,
+  isFetching,
+  error,
+});
