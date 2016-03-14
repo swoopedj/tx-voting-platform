899c966608da2b9c073fb976841e02f63ffd3f30
Dylan Swoope <swoopedj@gmail.com>
2016-03-14 11:33:28 -0500

    entry model work and youtube batch work

--- client/models/entry.js
+++ client/models/entry.js
@@ -1,5 +1,6 @@
 const request = require('../lib/request.js');
 
+
 const processData = (data) => {
   const videoData = data.data.items[0];
   const embedID = videoData.id;
@@ -17,14 +18,30 @@ const processData = (data) => {
 };
 
 const Entry = {
-  fetch: () => [],
+  fetch: () => {
+    return request.fetch('/api/yt/entries/')
+    .then(response => response.json())
+    .then(processData);
+  },
   create: (entry) => {
-    console.log(entry);
+    return request.fetch('/api/yt/entries/', {
+      method: 'POST',
+      body: JSON.srtingify(entry),
+    })
+    .then(response => response.json())
+    .then(processData);
   },
   getInfo: (url) => {
     return request.fetch(request.addParams('http://localhost:4000/api/yt/links/info', { url }))
       .then(response => response.json())
       .then(processData);
+  },
+  delete: (id) => {
+    return request.fetch(`/api/yt/entries/:${id}`, {
+      method: 'DELETE',
+    })
+    .then(response => response.json())
+    .then(processData);
   },
 };
 
