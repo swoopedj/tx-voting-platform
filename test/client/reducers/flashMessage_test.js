3b6824cfd9734b41742dca53dad8179503c2525e
Clay Branch <clay.branch@gmail.com>
2016-03-17 12:24:07 -0500

    added simple error handling test

--- test/client/reducers/flashMessage_test.js
+++ test/client/reducers/flashMessage_test.js
@@ -3,9 +3,25 @@
 require(TEST_HELPER);
 import Immutable from 'immutable';
 const { flashMessage }  = require(`${__client}/reducers/flashMessage`);
-const actions = require(`${__client}/actionCreators/flashMessage`);
+const entryActions = require(`${__client}/actionCreators/entries`);
 
 describe('The flash message reducer', () => {
+  const initialState = Immutable.fromJS({});
+  const error = { message: 'test', time: 1000 };
+  const getMessage = (action) => {
+    return flashMessage(initialState, action).toJS();
+  };
+  describe('generates error messages on', () => {
+    it.only('RECEIVE_NEW_ENTRY_ERROR', () => {
+      expect(getMessage(entryActions.receiveNewEntryError(error)))
+        .to.deep.equal({
+          type: 'warning',
+          message: 'Failed to create new entry',
+          createdAt: 1000,
+        });
+    });
+  });
+
   it('clears message when clear message is called', () => {
     const state = flashMessage(
         Immutable.fromJS({ message: 'test' }),
