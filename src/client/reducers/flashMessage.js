0f719484b6008b820b09666c7bf24e44e3bd1981
Clay Branch <clay.branch@gmail.com>
2016-03-17 13:47:09 -0500

    working through  messages

--- src/client/reducers/flashMessage.js
+++ src/client/reducers/flashMessage.js
@@ -1,9 +1,27 @@
 import Immutable from 'immutable';
 import { createReducer } from '../lib/redux-helpers';
 
+
+
+// if the getter is a function
+// passes the action into the getter
+// and returns the result, otherwise just returns the getter
+const actionWrap = (getter, action) => {
+  return typeof getter === 'function' ? getter(action) : getter;
+}
+
+
+const getFlashMessage = (action, getMessage, getTime, getType) => {
+  return Immutable.fromJS({
+    type : actionWrap(getType, action),
+    time: actionWrap(getTime, action),
+    message: actionWrap(getMessage, action),
+  })
+}
+
+const getError = (action, getErrorMessage) => getFlashMessage()
+
 export const flashMessage = createReducer(Immutable.fromJS({}), {
-  SET_FLASH_MESSAGE: (state, action) => {
-    return Immutable.fromJS(action.message);
-  },
+  RECEIVE_NEW_ENTRY_ERROR: () => Immutable.fromJS({}}
   CLEAR_FLASH_MESSAGE: () => Immutable.fromJS({}),
 });
