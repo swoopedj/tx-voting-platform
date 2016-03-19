a37f5285d8520ec608d5ac3a31a2daf709c3bb62
Justin Seiter <justinseiter@gmail.com>
2016-03-19 17:27:42 -0500

    Update profile view.

--- src/client/components/Profile.js
+++ src/client/components/Profile.js
@@ -2,21 +2,33 @@ import React from 'react';
 
 const Profile = () => {
   return (
-    <div className="columns">
-      <div className="column is-quarter">
-        <div className="card">
-          <div className="card-image">
-            <figure className="image is-square">
-              <img src="https://avatars2.githubusercontent.com/u/1420404?v=3&s=400" />
-            </figure>
+    <div>
+      <div className="profile-header">
+        <div className="columns">
+          <div className="column is-4">
+            <img className="image is-64x64 is-rounded is-pulled-left" src="https://avatars2.githubusercontent.com/u/1420404?v=3&s=400" />
+            <h1 className="title is-2">Clay Branch</h1>
           </div>
-          <div className="card-content">
-            <p className="title is-5">Clay Branch</p>
+          <div className="column is-5 is-offset-3">
+            <nav className="navbar">
+              <div className="navbar-item is-text-centered">
+                <p className="heading">Entries</p>
+                <p className="title">3</p>
+              </div>
+              <div className="navbar-item is-text-centered">
+                <p className="heading">Total Views</p>
+                <p className="title">2,123</p>
+              </div>
+              <div className="navbar-item is-text-centered">
+                <p className="heading">Total Likes</p>
+                <p className="title">45</p>
+              </div>
+            </nav>
           </div>
         </div>
       </div>
       <div className="column">
-        <h1 className="title">Submissions</h1>
+        <h1 className="title">Entries</h1>
         <hr />
       </div>
     </div>
