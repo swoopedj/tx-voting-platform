b5896db1bd179462c0b1579de9485f1ff3bb6912
Clay Branch <clay.branch@gmail.com>
2016-03-22 19:55:13 -0500

    made the logout view show if the user is logged in

--- src/client/components/Auth.js
+++ src/client/components/Auth.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import AuthUser from './AuthUser';
+import AuthLogin from './AuthLogin';
 
-const Auth = ({ onLogoutClick }) => {
-  return (
-    <AuthUser onLogoutClick={onLogoutClick} />
-  );
+const Auth = ({ onLogoutClick, user}) => {
+  if (user.isLoggedIn) return (<AuthUser onLogoutClick={onLogoutClick} userData={user.data} />);
+  return (<AuthLogin />);
 };
 
 module.exports = Auth;
