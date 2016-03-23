c44f5b268b31e9f945b39d2d60313e6d7e46231e
Clay Branch <clay.branch@gmail.com>
2016-03-22 19:56:26 -0500

    wired the AuthUser into the store

--- src/client/components/AuthUser.js
+++ src/client/components/AuthUser.js
@@ -2,17 +2,17 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const AuthUser = ({ onLogoutClick }) => {
+const AuthUser = ({ onLogoutClick, userData }) => {
   return (
     <span className="header-item user">
       <a className="user-link" href="#">
-        Clay
-        <img className="avatar header is-pulled-right" src="https://avatars2.githubusercontent.com/u/1420404?v=3&s=400" />
+        {userData.userName}
+        <img className="avatar header is-pulled-right" src={userData.photo} />
       </a>
       <ul>
         <li className="arrow-down"><i className="fa fa-sort-down arrow"></i></li>
         <li><Link to="/profile">My Profile<i className="fa fa-user"></i></Link></li>
-        <li><Link to="/logout" onClick={onLogoutClick} className="last" href="#">Logout<i className="fa fa-sign-out"></i></Link></li>
+        <li><Link to="/" onClick={onLogoutClick} className="last" href="#">Logout<i className="fa fa-sign-out"></i></Link></li>
       </ul>
     </span>
   );
